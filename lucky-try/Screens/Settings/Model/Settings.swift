3f130063a5ef1d3028b0c993299525990a09046b
MaciejDubowik <maciejxpoland@gmail.com>
2023-05-31 16:49:45 +0200

    create settings model

---
+++ lucky-try/Screens/Settings/Model/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  lucky-try
+//
+//  Created by Maciej Dubowik on 31/05/2023.
+//
+
+import Foundation
+
+struct Settings {
+    var numberOfPlayers: Int
+    var names: [String]
+    var bet: String
+}
